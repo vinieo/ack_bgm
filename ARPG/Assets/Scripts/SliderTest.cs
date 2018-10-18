8aebd723a297eed14cc9afb213c28e752d4e96a3
vinieo <374863506@qq.com>
2018-10-19 00:37:50 +0800

    game

---
+++ ARPG/Assets/Scripts/SliderTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderTest : MonoBehaviour {
+	public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void GetSliderValue(float value){
+		audio.volume = value;
+	}
+}
